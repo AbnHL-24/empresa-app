c5bf0440aab7a9ea249965bfc91bb7aaebc27006
AbnHL-24 <abner1269@outlook.com>
2025-03-16 08:57:13 -0600

    Fix Insercion de dependencias.

--- EmpresaAPI/Program.cs
+++ EmpresaAPI/Program.cs
@@ -2,19 +2,31 @@ using EmpresaAPI.DbContext;
 using Microsoft.EntityFrameworkCore;
 //using Microsoft.EntityFrameworkCore.Design;
 using EmpresaAPI.Models.Departamento;
+using EmpresaAPI.Repository.Departamento;
+using EmpresaAPI.Repository.Empleado;
+using EmpresaAPI.Services.Departamento;
+using EmpresaAPI.Services.Empleado;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.Blazor;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddDbContext<EmpresaApiContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Regostro de servicios (en builkder.Service).
+builder.Services.AddScoped<IDepartamentoRepositorio, DepartamentoRepositorio>();
+builder.Services.AddScoped<IEmpleadoRepositorio, EmpleadoRepositorio>();
+builder.Services.AddScoped<IDepartamentoServicio, DepartamentoServicio>();
+builder.Services.AddScoped<IEmpleadoServicio, EmpleadoServicio>();
+/*builder.Services.AddTransient<IDepartamentoServicio, DepartamentoServicio>();
+builder.Services.AddTransient<IEmpleadoServicio, EmpleadoServicio>();*/
 
 var app = builder.Build();
 
