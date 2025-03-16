0dc5eef3c13d9584cc9fd0e5bc89da5899a904dd
AbnHL-24 <abner1269@outlook.com>
2025-03-16 15:10:00 -0600

    Refactorizacion de IDepartamentoRepositorio.cs

--- EmpresaAPI/Repository/Departamento/IDepartamentoRepositorio.cs
+++ EmpresaAPI/Repository/Departamento/IDepartamentoRepositorio.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 public interface IDepartamentoRepositorio
 {
     Task<IEnumerable<DepartamentoModel>> ObtenerDepartamentosAsync();
-    Task<DepartamentoModel> ObtenerDepartamentoPorIdAsync(int id);
+    Task<DepartamentoModel?> ObtenerDepartamentoPorIdAsync(int id);
+    Task<DepartamentoModel?> ObtenerDepartamentoPorNombreAsync(string nombre);
     Task CrearAsync(DepartamentoModel departamento);
     Task ActualizarAsync(DepartamentoModel departamento);
 }
