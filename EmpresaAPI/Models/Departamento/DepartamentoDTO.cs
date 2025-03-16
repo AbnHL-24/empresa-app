c58b17e9fcae21c41d68e4384a1b35cdf23c9f44
AbnHL-24 <abner1269@outlook.com>
2025-03-16 00:58:28 -0600

    Actualizaciones de la API

--- EmpresaAPI/Models/Departamento/DepartamentoDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EmpresaAPI.Controllers;
-
-public class DepartamentoDTO
-{
-    public int IdDepartamento { get; set; }
-    public string Nombre { get; set; }
-    public string Presupuesto { get; set; }
-}
