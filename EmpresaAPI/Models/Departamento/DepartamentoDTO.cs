ef9aacffb4b77922210077a7551af241b5440631
AbnHL-24 <abner1269@outlook.com>
2025-03-15 22:31:26 -0600

    Add DTO's

---
+++ EmpresaAPI/Models/Departamento/DepartamentoDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace EmpresaAPI.Controllers;
+
+public class DepartamentoDTO
+{
+    public int IdDepartamento { get; set; }
+    public string Nombre { get; set; }
+    public string Presupuesto { get; set; }
+}
