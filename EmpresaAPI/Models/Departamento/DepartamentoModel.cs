4749547e6e89ad3fb3bea72f7146df319a75e31c
AbnHL-24 <abner1269@outlook.com>
2025-03-15 22:30:37 -0600

    Add models.

---
+++ EmpresaAPI/Models/Departamento/DepartamentoModel.cs
@@ -0,0 +1,8 @@
+﻿namespace EmpresaAPI.Models.Departamento;
+
+public class DepartamentoModel
+{
+    public int IdDepartamento { get; set; }
+    public string Nombre { get; set; }
+    public string Presupuesto { get; set; }
+}
---
+++ EmpresaAPI/Models/Empleado/EmpleadoModel.cs
@@ -0,0 +1,17 @@
+﻿namespace EmpresaAPI.Models.Empleado;
+
+public class EmpleadoModel
+{
+    public long CuiEmpleado { get; set; }
+    public string Nombre { get; set; }
+    public string Apellidos { get; set; }
+    public string? Telefono { get; set; }
+    public string? correo { get; set; }
+    public string usuario { get; set; }
+    public string contrasenya { get; set; }
+    public int Puesto { get; set; }
+    public double Salario { get; set; }
+    public DateOnly? UltimoAumento { get; set; }
+    public DateOnly FechaIngreso { get; set; }
+    public DateOnly FechaBaja { get; set; }
+}
