22c971416a3d891d79bbf2d1770fce4befe9bdf3
AbnHL-24 <abner1269@outlook.com>
2025-03-16 00:56:41 -0600

    Agregar repositorio de departamento.

---
+++ EmpresaAPI/Repository/Departamento/DepartamentoRepositorio.cs
@@ -0,0 +1,37 @@
+﻿using EmpresaAPI.DbContext;
+using EmpresaAPI.Models.Departamento;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmpresaAPI.Repository.Departamento;
+
+public class DepartamentoRepositorio : IDepartamentoRepositorio
+{
+    private readonly EmpresaApiContext _context;
+
+    public DepartamentoRepositorio(EmpresaApiContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<IEnumerable<DepartamentoModel>> ObtenerDepartamentosAsync()
+    {
+        return await _context.Departamentos.ToListAsync();
+    }
+
+    public async Task<DepartamentoModel> ObtenerDepartamentoPorIdAsync(int id)
+    {
+        return await _context.Departamentos.FindAsync(id);
+    }
+
+    public async Task CrearAsync(DepartamentoModel departamento)
+    {
+        await _context.Departamentos.AddAsync(departamento);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task ActualizarAsync(DepartamentoModel departamento)
+    {
+        _context.Departamentos.Update(departamento);
+        await _context.SaveChangesAsync();
+    }
+}
---
+++ EmpresaAPI/Repository/Departamento/IDepartamentoRepositorio.cs
@@ -0,0 +1,12 @@
+﻿using EmpresaAPI.Models.Departamento;
+
+namespace EmpresaAPI.Repository.Departamento;
+using System.Threading.Tasks;
+
+public interface IDepartamentoRepositorio
+{
+    Task<IEnumerable<DepartamentoModel>> ObtenerDepartamentosAsync();
+    Task<DepartamentoModel> ObtenerDepartamentoPorIdAsync(int id);
+    Task CrearAsync(DepartamentoModel departamento);
+    Task ActualizarAsync(DepartamentoModel departamento);
+}
