a59bd8cc56590dc5ba4f933e85f05bdac9529155
AbnHL-24 <abner1269@outlook.com>
2025-03-16 02:00:08 -0600

    Repositorios de empleado agregados.

---
+++ EmpresaAPI/Repository/Empleado/EmpleadoRepositorio.cs
@@ -0,0 +1,37 @@
+﻿using EmpresaAPI.DbContext;
+using EmpresaAPI.Models.Empleado;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmpresaAPI.Repository.Empleado;
+
+public class EmpleadoRepositorio : IEmpleadoRepositorio
+{
+    private readonly EmpresaApiContext _context;
+    
+    public EmpleadoRepositorio(EmpresaApiContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<IEnumerable<EmpleadoModel>> ObtenerEmpleadosAsync()
+    {
+        return await _context.Empleados.ToListAsync();
+    }
+
+    public async Task<EmpleadoModel> ObtenerEmpleadoAsync(int id)
+    {
+        return await _context.Empleados.FindAsync(id);
+    }
+
+    public async Task CrearEmpleadoAsync(EmpleadoModel empleado)
+    {
+        await _context.Empleados.AddAsync(empleado);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task ActualizarEmpleadoAsync(EmpleadoModel empleado)
+    {
+        _context.Empleados.Update(empleado);
+        await _context.SaveChangesAsync();
+    }
+}
---
+++ EmpresaAPI/Repository/Empleado/IEmpleadoRepositorio.cs
@@ -0,0 +1,11 @@
+﻿using EmpresaAPI.Models.Empleado;
+
+namespace EmpresaAPI.Repository.Empleado;
+
+public interface IEmpleadoRepositorio
+{
+    Task<IEnumerable<EmpleadoModel>> ObtenerEmpleadosAsync();
+    Task<EmpleadoModel> ObtenerEmpleadoAsync(int id);
+    Task CrearEmpleadoAsync(EmpleadoModel empleado);
+    Task ActualizarEmpleadoAsync(EmpleadoModel empleado);
+}
