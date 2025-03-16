c28425e298b554a01f01546443fd979aa79cab9d
AbnHL-24 <abner1269@outlook.com>
2025-03-16 00:57:53 -0600

    Actualizacion de clase modelo de Departamento.

--- EmpresaAPI/Models/Departamento/DepartamentoModel.cs
+++ EmpresaAPI/Models/Departamento/DepartamentoModel.cs
@@ -1,8 +1,20 @@
-﻿namespace EmpresaAPI.Models.Departamento;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace EmpresaAPI.Models.Departamento;
 
 public class DepartamentoModel
 {
+    [Key]
+    [JsonPropertyName("id_departamento")]
     public int IdDepartamento { get; set; }
+    
+    [Required(ErrorMessage = "El nombre es obligatorio")]
+    [StringLength(75, ErrorMessage = "El nombre debe tener menos de 75 caracteres")]
+    [JsonPropertyName("nombre")]
     public string Nombre { get; set; }
+    
+    [Range(0, double.MaxValue, ErrorMessage = "El presupuesto debe ser mayor o igual a 0")]
+    [JsonPropertyName("presupuesto")]
     public string Presupuesto { get; set; }
 }
