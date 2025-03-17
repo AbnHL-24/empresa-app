c12f9f162deccd2108618ce710993f1950f25a87
AbnHL-24 <abner1269@outlook.com>
2025-03-17 01:10:21 -0600

    add modelos

---
+++ EmpresaWebApp/Models/Departamento/DepartamentoModel.cs
@@ -0,0 +1,8 @@
+﻿namespace EmpresaWebApp.Models.Departamento;
+
+public class DepartamentoModel
+{
+    public int IdDepartamento { get; set; }
+    public string Nombre { get; set; }
+    public decimal Presupuesto { get; set; }
+}
